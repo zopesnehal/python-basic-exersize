d95a3f1a763bc3d0bc38db55ea7677751bdb93f6
Nikhil Rane <nikhilrane1992@gmail.com>
2019-08-21 21:33:58 +0530

    learn opps concept

---
+++ oop.py
@@ -0,0 +1,10 @@
+class Acount :
+	pass 
+# 1. Instantiate the class
+acct1 = Account('jose',100)
+# 2.print the oblect 
+print(acc1)
+Account owner: jose
+Account balance:$100
+# 3.show the account owner attribute
+acct1.owner
